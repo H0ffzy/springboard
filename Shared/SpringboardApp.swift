3e8d9b6618533266d18a5ca7fa572ec482428733
Adam Hoffman <adam@gravitybit.com>
2022-03-24 12:17:12 -0400

    Initial Commit

---
+++ Shared/SpringboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpringboardApp.swift
+//  Shared
+//
+//  Created by Adam Hoffman on 3/24/22.
+//
+
+import SwiftUI
+
+@main
+struct SpringboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
